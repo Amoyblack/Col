be6caddd991905f9036bdba72f58a8c4cb762f66
amoyblack <flyoverrs@outlook.com>
2022-11-15 14:12:37 +0800

    10.0.10 Release
    添加版本号

--- DefaultSetting.lua
+++ DefaultSetting.lua
@@ -4,7 +4,7 @@ local _, rs = ...
 
 
 rs.V.DefaultSetting = {
-	["Version"] = "10.0.07",
+	["Version"] = "10.0.10",
     ["ForceUpdate"] = false,
     ["ShowMiniMapBtn"] = true,
     ["BarTexture"] = "s2",
